8ee0e396cef01007dd859f626da8e7d40fafad3f
Omar <omar1708@hotmail.com>
2024-09-20 16:40:38 -0400

    Ok

---
+++ DoctorBackend/Models/DTOs/EspecialidadDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs
+{
+    public class EspecialidadDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(60, MinimumLength = 1, ErrorMessage = "El nombre debe ser minimo 1 y maximo 60 caracteres")]
+        public string NombreEspecialidad { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es requerida")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "La descripcion debe ser minimo 1 y maximo 60 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public int Estado { get; set; } // 1 - 0
+    }
+}
